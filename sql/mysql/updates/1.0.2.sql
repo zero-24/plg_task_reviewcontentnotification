bd422c4154f488f549071f30b4c957385b8eabe6
zero-24 <zero-24@users.noreply.github.com>
2025-01-13 23:55:35 +0100

    fix alter table calls

--- sql/mysql/updates/1.0.2.sql
+++ sql/mysql/updates/1.0.2.sql
@@ -6,5 +6,5 @@ INSERT IGNORE INTO `#__mail_templates` (`template_id`, `extension`, `language`, 
 ('plg_task_reviewcontentnotification.second_notification_mail', 'plg_task_reviewcontentnotification', '', 'PLG_TASK_REVIEWCONTENTNOTIFICATION_SECOND_NOTIFICATION_MAIL_SUBJECT', 'PLG_TASK_REVIEWCONTENTNOTIFICATION_SECOND_NOTIFICATION_MAIL_BODY', '', '', '{"tags": ["title", "public_url", "sitename", "url", "last_modified", "created", "edit_url", "backend_url", "date_modifier"]}');
 
 -- Rename the column
-ALTER TABLE "#__content_reviewcontentnotification" RENAME COLUMN "seccond_notification" TO "second_notification";
-ALTER TABLE "#__content_reviewcontentnotification" RENAME COLUMN "seccond_notification_send" TO "second_notification_send";
+ALTER TABLE `#__content_reviewcontentnotification` CHANGE COLUMN seccond_notification second_notification datetime;
+ALTER TABLE `#__content_reviewcontentnotification` CHANGE COLUMN seccond_notification second_notification datetime;
